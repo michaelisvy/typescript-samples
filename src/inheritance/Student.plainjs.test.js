e6c2f1a290a5c23d75d5b474e70d983a6bd3667f
Michael <michael.isvy@online-pajak.com>
2019-08-23 09:06:57 +0800

    unstable experiment on js-ts interaction

---
+++ src/inheritance/Student.plainjs.test.js
@@ -0,0 +1,9 @@
+const Student = require('./Student.ts');
+const Person = require('./Person');
+
+
+test('sum', () => {
+    console.log(Student);
+  //const studentJohn = new Student(15, "Harry John", 33);
+  //expect(studentJohn.getAge()).toEqual(15);
+});
